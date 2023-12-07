445a7371974249bd4cd13a38a25e5b2360593749
suryasaranteja <145064324+suryasaranteja@users.noreply.github.com>
2023-12-07 00:17:46 -0500

    Update index.js

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Tabs } from "antd";
 import Projects from "./Projects";
+import { useDispatch, useSelector } from "react-redux";
 
 function Profile() {
+  const { user } = useSelector((state) => state.users);
   return (
     <Tabs defaultActiveKey="1">
       <Tabs.TabPane tab="Projects" key="1">
         <Projects />
       </Tabs.TabPane>
       <Tabs.TabPane tab="General" key="2">
-        General
+        <br></br><br></br>
+        <center>
+        <h1>Employee Name : {user?.firstName} {user?.lastName} </h1>
+        <br></br>
+        <h1>Company Name: WDG29 Solutions Pvt Ltd</h1>
+        </center>
       </Tabs.TabPane>
     </Tabs>
   );
